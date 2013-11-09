a27ea18e9a361d8339087151c44795912bf12ad1
Michael Haag <michael.haag@ahorngmbh.de>
2013-11-09 17:45:21 +0100

    added preferred language detection

--- lib/globalize2/site_controller_extensions.rb
+++ lib/globalize2/site_controller_extensions.rb
@@ -4,10 +4,27 @@ module Globalize2
       base.send(:include, InstanceMethods)
       base.class_eval do
         alias_method_chain :find_page, :globalize
+        alias_method_chain :show_page, :homepage_redir
         before_filter :set_locale
       end
     end
 
+    def show_page_with_homepage_redir  
+      url = params[:url]  
+      if Array === url  
+        url = url.join('/')  
+      else  
+        url = url.to_s  
+      end  
+  
+      if url == '/'  
+        preferred = request.env.http_accept_language.preferred_language_from(Globalize2Extension.locales)
+        locale = params[:locale] || cookies[:locale] || session[:locale] || preferred || Globalize2Extension.default_language
+        redirect_to CGI.unescape('/' + locale + '/') and return  
+      end  
+  
+      show_page_without_homepage_redir  
+    end 
 
     module InstanceMethods
       def set_locale
